6e7286deb99af1e14a47897881ff304d4b83d818
Roberto Gomez <rcgomezap@gmail.com>
2025-01-13 09:03:32 -0500

    Updated to 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [view, toggleView] = useState(false)
+
+  const style = {
+    borderStyle: 'solid',
+    margin: '10px',
+    padding: '5px'
+  }
+
+  return (
+  <div style={style}>
+    {blog.title} 
+    {view ? ( 
+      <>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button>like</button> </p> 
+        <p>{blog.author}</p>
+      </>
+
+    ) : (
+      <button onClick={toggleView}>view</button>
+    )}
+  </div>
+  )  
+}
 
 export default Blog
